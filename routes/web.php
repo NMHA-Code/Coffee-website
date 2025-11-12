937feb158126deba862467c97b19f1a08c4527f1
nguyenmauhaianh <codemau140804@gmail.com>
2025-11-12 23:24:47 +0700

    Khung du an

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+// Router Users
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Categories
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Products
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Inventorys
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Orders
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Orders_item
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
+// Router Suppliers
+Route::get('/', function () {
+    return 'xin chao laravel nhe';
+});
